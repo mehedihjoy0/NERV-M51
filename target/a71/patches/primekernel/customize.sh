da6207719cbc08ab26717576b3c78a878d10e888
Pascua <32544529+pascua28@users.noreply.github.com>
2025-08-16 23:41:53 +0800

    target: a71: misc fixes
    * target: a71: remove r9q libs dependency
    
    Remove leftover fixes from early porting.
    
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>
    
    * target: a71: primekernel: download latest upstream release from github
    
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>
    
    ---------
    
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- target/a71/patches/primekernel/customize.sh
+++ target/a71/patches/primekernel/customize.sh
@@ -1,6 +1,18 @@
-CURR_DIR=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
+SKIPUNZIP=1
 
-LOG_STEP_IN "Copying boot.img and dtbo.img"
-cp $CURR_DIR/kernel/boot.img $WORK_DIR/kernel/boot.img
-cp $CURR_DIR/kernel/dtbo.img $WORK_DIR/kernel/dtbo.img
-LOG_STEP_OUT
+# [
+KERNEL_REPO="https://github.com/pascua28/android_kernel_samsung_sm7150/releases/download/upstream"
+
+REPLACE_KERNEL_BINARIES()
+ {
+     [ -f "$WORK_DIR/kernel/boot.img" ] && rm -rf "$WORK_DIR/kernel/boot.img"
+     echo "Downloading boot.img"
+     curl -L -s -o "$WORK_DIR/kernel/boot.img" "$KERNEL_REPO/boot.img"
+
+     [ -f "$WORK_DIR/kernel/dtbo.img" ] && rm -rf "$WORK_DIR/kernel/dtbo.img"
+     echo "Downloading dtbo.img"
+     curl -L -s -o "$WORK_DIR/kernel/dtbo.img" "$KERNEL_REPO/dtbo.img"
+ }
+# ]
+
+REPLACE_KERNEL_BINARIES

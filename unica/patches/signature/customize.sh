e7853c290727ecfe7de99aee4774acfe6b94b31d
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2025-08-18 13:40:05 +0300

    unica: mods: add multiple mods from ExtremeROM
    Change-Id: I8da0f3173ed0ef5cd63bd854f8f60dd00396b83f
    Signed-off-by: Ksawlii <maja.blaszczak@proton.me>

--- unica/patches/signature/customize.sh
+++ unica/patches/signature/customize.sh
@@ -1,5 +1,5 @@
 CERT_PREFIX="aosp"
-$ROM_IS_OFFICIAL && CERT_PREFIX="unica"
+$ROM_IS_OFFICIAL && CERT_PREFIX="nerv"
 
 CERT_SIGNATURE=$(cat "$SRC_DIR/security/${CERT_PREFIX}_platform.x509.pem" | \
     sed '/CERTIFICATE/d' | tr -d '\n' | base64 -d | xxd -p -c 0)

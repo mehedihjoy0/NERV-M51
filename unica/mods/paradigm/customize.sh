b6b043921d3486f32cc438b8710ededaf48295cf
majaahh <maja.blaszczak@proton.me>
2025-08-27 14:29:51 +0200

    unica: patches: miscs: Move ff mods for Now brief to paradigm
    Change-Id: Ib393b4a0b6bbe0fa18568c95d67bf2af55bd41b1
    Signed-off-by: majaahh <maja.blaszczak@proton.me>

--- unica/mods/paradigm/customize.sh
+++ unica/mods/paradigm/customize.sh
@@ -2,4 +2,6 @@ LOG_STEP_IN "- Adding Now Brief"
 ADD_TO_WORK_DIR "pa1qxx" "system" "system/priv-app/SamsungSmartSuggestions/SamsungSmartSuggestions.apk" 0 0 644 "u:object_r:system_file:s0"
 ADD_TO_WORK_DIR "pa1qxx" "system" "system/etc/default-permissions/default-permissions-com.samsung.android.app.moments.xml" 0 0 644 "u:object_r:system_file:s0"
 ADD_TO_WORK_DIR "pa1qxx" "system" "system/priv-app/Moments/Moments.apk" 0 0 644 "u:object_r:system_file:s0"
+SET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_COMMON_CONFIG_AI_VERSION" "20251"
+SET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_FRAMEWORK_SUPPORT_PERSONALIZED_DATA_CORE" "TRUE"
 LOG_STEP_OUT

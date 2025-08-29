4fc94759616107ed2c1f9454d90f7473bd368219
Majaahh <maja.blaszczak@proton.me>
2025-08-29 11:57:44 +0200

    target: a53x: floppy: Minor adjustments
    Change-Id: If936e41551768d715a1a9e681288a7498c1ac19a
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- target/a53x/patches/floppy/customize.sh
+++ target/a53x/patches/floppy/customize.sh
@@ -1,24 +1,19 @@
-LATEST=$(curl -s https://api.github.com/repos/FlopKernel-Series/flop_s5e8825_kernel/releases/latest)
-FLOPPY_TAR=$(echo "$LATEST" |
-    jq -r '.assets[] | select(.name | test("OneUI.*Vanilla-exynos1280.*\\.tar$")) | .browser_download_url')
+FLOPPY_TAR=$(curl -s https://api.github.com/repos/FlopKernel-Series/flop_s5e8825_kernel/releases/latest \
+    | jq -r 'del(.body) | .assets[] | select(.name | test("OneUI.*Vanilla-exynos1280.*\\.tar$")) | .browser_download_url')
 
-[ -d "$TMP_DIR" ] && rm -rf "$TMP_DIR"
+[[ -d "$TMP_DIR" ]] && rm -rf "$TMP_DIR"
 mkdir -p "$TMP_DIR"
 
-DOWNLOAD_FILE "$FLOPPY_TAR" "$TMP_DIR/floppy.tar"
+DOWNLOAD_FILE "$FLOPPY_TAR" "$TMP_DIR/$(basename $FLOPPY_TAR)"
 
 LOG "- Extracting kernel binaries"
-EVAL "tar xvf \"$TMP_DIR/floppy.tar\" -C \"$TMP_DIR\""
+EVAL "tar xvf \"$TMP_DIR/$(basename $FLOPPY_TAR)\" -C \"$TMP_DIR\""
 EVAL "lz4 -d \"$TMP_DIR/boot.img.lz4\" \"$TMP_DIR/boot.img\""
 EVAL "lz4 -d \"$TMP_DIR/vendor_boot.img.lz4\" \"$TMP_DIR/vendor_boot.img\""
 
-LOG_STEP_IN "- Replacing kernel binaries"
-for i in "boot" "vendor_boot"; do
-    (
-        [ -f "$WORK_DIR/kernel/$i.img" ] && rm -rf "$WORK_DIR/kernel/$i.img"
-        mv -f "$TMP_DIR/$i.img" "$WORK_DIR/kernel/$i.img"
-    ) &
-done
-LOG_STEP_OUT
+LOG "- Replacing kernel binaries"
+[[ -f "$WORK_DIR/kernel/boot.img" ]] && rm -f "$WORK_DIR/kernel/boot.img"
+[[ -f "$WORK_DIR/kernel/boot.img" ]] && rm -f "$WORK_DIR/kernel/vendor_boot.img"
+EVAL "mv -f \"$TMP_DIR/boot.img\" \"$TMP_DIR/vendor_boot.img\" \"$WORK_DIR/kernel\"" || exit 1
 
 rm -rf "$TMP_DIR"

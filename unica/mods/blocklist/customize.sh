1f4dbed241aa15c1267883fe0f7704b5ded05fbe
Salvo Giangreco <giangrecosalvo9@gmail.com>
2025-09-06 18:55:57 +0200

    unica: mods: blocklist: fix patch not applying issue
    Change-Id: Id3bc5da0484590ae28cb090e8b85f77cd999d022

--- unica/mods/blocklist/customize.sh
+++ unica/mods/blocklist/customize.sh
@@ -1,1 +1,2 @@
 DELETE_FROM_WORK_DIR "system" "system/etc/ldu_blocklist.xml"
+rm "$APKTOOL_DIR/system/framework/services.jar/smali_classes2/com/samsung/android/server/pm/install/PackageBlockListPolicy\$1.smali"
